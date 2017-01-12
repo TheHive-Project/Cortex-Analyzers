374d6180d169fce7d2f06369df7881c5bbd46d59
Nabil Adouani <nabil.adouani@gmail.com>
2017-01-12 17:39:53 +0100

    Add more metadata to cortexutils library

--- contrib/setup.py
+++ contrib/setup.py
@@ -2,7 +2,13 @@ from setuptools import setup
 
 setup(
     name='cortexutils',
-    version='0.1',
+    version='1.0.0',
     description='Cortext Python utilities package',
-    py_modules=['cortexutils.analyzer']
+    author='TheHive-Project',
+    author_email='support@thehive-project.org',
+    url='https://github.com/CERT-BDF/Cortex-Analyzers/tree/master/contrib',
+    py_modules=['cortexutils.analyzer'],
+    install_requires=[
+        'ioc-parser'
+    ]
 )
