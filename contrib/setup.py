9d4c55da0dcb709d13c4f0223c4aa112d38dcf04
Nabil Adouani <nabil.adouani@gmail.com>
2017-01-13 16:41:38 +0100

    Add cortexutils pip descriptor

--- contrib/setup.py
+++ contrib/setup.py
@@ -3,10 +3,23 @@ from setuptools import setup
 setup(
     name='cortexutils',
     version='1.0.0',
-    description='Cortext Python utilities package',
+    description='A Python library for including utility classes for Cortex analyzers',
+    long_description=open('README').read(),
     author='TheHive-Project',
     author_email='support@thehive-project.org',
+    license='AGPL-V3',
     url='https://github.com/CERT-BDF/Cortex-Analyzers/tree/master/contrib',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
     py_modules=['cortexutils.analyzer'],
     install_requires=[
         'ioc-parser'
