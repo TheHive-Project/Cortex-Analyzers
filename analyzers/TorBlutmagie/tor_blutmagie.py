f307340bc121c1e19282d2bc748d50a996216e7e
srilumpa <marcandre.doll@gmail.com>
2017-11-02 09:11:51 +0100

    Add documentation to class

--- analyzers/TorBlutmagie/tor_blutmagie.py
+++ analyzers/TorBlutmagie/tor_blutmagie.py
@@ -4,7 +4,20 @@ import csv
 
 
 class TorBlutmagieClient:
-    """docstring for TorBlutmagieClient"""
+    """Simple client to query torstatus.blutmagie.de for exit nodes.
+
+    The client will download http://torstatus.blutmagie.de/query_export.php/Tor_query_EXPORT.csv
+    and check if a specified IP address, FQDN or domain is present in it.
+    It will cache the response for `cache_duration` seconds to avoid
+    too much latency.
+
+    :param cache_duration: Duration before refreshing the cache (in seconds).
+                           Ignored if `cache_duration` is 0.
+    :param cache_root: Path where to store the cached file
+                       downloaded from torstatus.blutmagie.de
+    :type cache_duration: int
+    :type cache_root: str
+    """
     def __init__(self, cache_duration=3600, cache_root='/tmp/cortex/tor_project'):
         self.session = requests.Session()
         if cache_duration > 0:
@@ -58,6 +71,26 @@ class TorBlutmagieClient:
         return results
 
     def search_tor_node(self, data_type, data):
+        """Lookup an artifact to check if it is a known tor exit node.
+
+        :param data_type: The artifact type. Must be one of 'ip', 'fqdn'
+                          or 'domain'
+        :param data: The artifact to lookup
+        :type data_type: str
+        :type data: str
+        :return: Data relative to the tor node. If the looked-up artifact is
+                 related to a tor exit node it will contain a `nodes` array.
+                 That array will contains a list of nodes containing the
+                 following keys:
+                 - name: name given to the router
+                 - ip: their IP address
+                 - hostname: Hostname of the router
+                 - country_code: ISO2 code of the country hosting the router
+                 - as_name: ASName registering the router
+                 - as_number: ASNumber registering the router
+                  Otherwise, `nodes` will be empty.
+        :rtype: list
+        """
         results = []
         if data_type == 'ip':
             results = self._get_node_from_ip(data)
