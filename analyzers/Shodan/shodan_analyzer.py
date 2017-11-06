b33e526b0cf904b66495856fbf33c6d6a3c23b51
Nabil Adouani <nabil.adouani@gmail.com>
2017-11-06 11:14:43 +0100

    #106 Review Shodan analyzer

--- analyzers/Shodan/shodan_analyzer.py
+++ analyzers/Shodan/shodan_analyzer.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 from cortexutils.analyzer import Analyzer
 from shodan_api import ShodanAPIPublic
+from shodan.exception import APIError
 
 
 class ShodanAnalyzer(Analyzer):
@@ -24,27 +25,44 @@ class ShodanAnalyzer(Analyzer):
                 value = raw['host']['country_name']
                 taxonomies.append(self.build_taxonomy(level, namespace, predicate, value))
             if 'org' in raw['host']:
-                taxonomies.append(self.build_taxonomy(level, namespace, 'org', raw['host']['org']))
+                taxonomies.append(self.build_taxonomy(level, namespace, 'Org', raw['host']['org']))
             if 'asn' in raw['host']:
-                taxonomies.append(self.build_taxonomy(level, namespace, 'asn', raw['host']['asn']))
+                taxonomies.append(self.build_taxonomy(level, namespace, 'ASN', raw['host']['asn']))
         elif self.service == 'search':
-                taxonomies.append(self.build_taxonomy(level,namespace, 'search', 'OSINT'))
+            if 'ips' in raw['infos_domain']:
+                value = "\"{}\"".format(len(raw['infos_domain']['ips']))
+                taxonomies.append(self.build_taxonomy(level, namespace, 'IPs', value))
+            if 'all_domains' in raw['infos_domain']:
+                value = "\"{}\"".format(len(raw['infos_domain']['all_domains']))
+                taxonomies.append(self.build_taxonomy(level, namespace, 'Domains', value))
+            if 'asn' in raw['infos_domain']:
+                value = "\"{}\"".format(len(raw['infos_domain']['asn']))
+                taxonomies.append(self.build_taxonomy(level, namespace, 'ASNs', value))
+            if 'isp' in raw['infos_domain']:
+                value = "\"{}\"".format(len(raw['infos_domain']['isp']))
+                taxonomies.append(self.build_taxonomy(level, namespace, 'ISPs', value))
+
         return {'taxonomies': taxonomies}
 
     def run(self):
         Analyzer.run(self)
 
-        self.shodan_client = ShodanAPIPublic(self.shodan_key)
-        if self.service == 'host':
-            ip = self.getParam('data', None, 'Data is missing')
-            results = {'reverse_dns': {'hostnames': self.shodan_client.reverse_dns(ip)[ip]},
-                       'host': self.shodan_client.host(ip)}
-            self.report(results)
-        if self.service == 'search':
-            domain = self.getParam('data', None, 'Data is missing')
-            result = {'dns_resolve': self.shodan_client.dns_resolve(domain),
-                      'infos_domain': self.shodan_client.info_domains(domain)}
-            self.report(result)
+        try:
+            self.shodan_client = ShodanAPIPublic(self.shodan_key)
+            if self.service == 'host':
+                ip = self.getParam('data', None, 'Data is missing')
+                results = {'reverse_dns': {'hostnames': self.shodan_client.reverse_dns(ip)[ip]},
+                           'host': self.shodan_client.host(ip)}
+                self.report(results)
+            if self.service == 'search':
+                domain = self.getParam('data', None, 'Data is missing')
+                result = {'dns_resolve': self.shodan_client.dns_resolve(domain),
+                          'infos_domain': self.shodan_client.info_domains(domain)}
+                self.report(result)
+        except APIError as e:
+            self.error(str(e))
+        except Exception as e:
+            self.unexpectedError(e)
 
 
 if __name__ == '__main__':
