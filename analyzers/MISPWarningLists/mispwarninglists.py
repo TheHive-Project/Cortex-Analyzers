d1e8b7288fccc8564dc9feef4631b2865c7bba9c
nk <Nils.Kuhnert@cert-bund.de>
2017-11-07 15:21:57 +0100

    Added commit hash check for warninglists repo

--- analyzers/MISPWarningLists/mispwarninglists.py
+++ analyzers/MISPWarningLists/mispwarninglists.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
 import io
 import json
+import requests
 
 from cortexutils.analyzer import Analyzer
+from cortexutils.extractor import Extractor
 from glob import glob
 from os.path import exists
 
 
 class MISPWarninglistsAnalyzer(Analyzer):
+    """
+    This analyzer compares given data to the MISP warning lists obtainable via
+    https://github.com/MISP/misp-warninglists.
+    Configuration options are:
+
+    ```
+    MISPWarningLists {
+      path = "/path/to/misp-warninglists/repository"  # Default: "misp-warninglists"
+    }
+    ```
+    """
     def __init__(self):
         Analyzer.__init__(self)
 
@@ -15,40 +28,38 @@ class MISPWarninglistsAnalyzer(Analyzer):
         self.path = self.get_param('config.path', 'misp-warninglists')
         if not exists(self.path):
             self.error('Path to misp-warninglists does not exist.')
-        self.warninglists = self.__readwarninglists()
+        self.warninglists = self.readwarninglists()
 
-    def __readwarninglists(self):
+    def readwarninglists(self):
         files = glob('{}/lists/*/*.json'.format(self.path))
         listcontent = []
         for file in files:
             with io.open(file, 'r') as fh:
                 content = json.loads(fh.read())
+                values = Extractor().check_iterable(content.get('list', []))
                 obj = {
                     "name": content.get('name', 'Unknown'),
-                    "values": content.get('list', []),
-                    "dataTypes": []
+                    "values": [value['value'] for value in values],
+                    "dataTypes": [value['type'] for value in values]
                 }
-                for type in content.get('matching_attributes', []):
-                    if type in ['md5', 'sha1', 'sha256', 'ssdeep']:
-                        obj['dataTypes'].append('hash')
-                        continue
-                    if 'filename|' in type:
-                        obj['dataTypes'].append('hash')
-                        continue
-                    if 'ip' in type:
-                        obj['dataTypes'].append('ip')
-                        continue
-                    if 'domain' in type:
-                        obj['dataTypes'].append('domain')
-                        continue
-                    if 'url' in type:
-                        obj['dataTypes'].append('url')
                 listcontent.append(obj)
         return listcontent
 
-    def __lastcommit(self):
-        with io.open('{}/.git/refs/heads/master'.format(self.path), 'r') as fh:
-            return fh.read()
+    def lastlocalcommit(self):
+        try:
+            with io.open('{}/.git/refs/heads/master'.format(self.path), 'r') as fh:
+                return fh.read().strip('\n')
+        except Exception as e:
+            return 'Error: could not get local commit hash ({}).'.format(e)
+
+    @staticmethod
+    def lastremotecommit():
+        url = 'https://api.github.com/repos/misp/misp-warninglists/branches/master'
+        try:
+            result_dict = requests.get(url).json()
+            return result_dict['commit']['sha']
+        except Exception as e:
+            return 'Error: could not get remote commit hash ({}).'.format(e)
 
     def run(self):
         results = []
@@ -58,13 +69,13 @@ class MISPWarninglistsAnalyzer(Analyzer):
 
             if self.data in list.get('values', []):
                 results.append({
-                    'name': list.get('name')
+                    "name": list.get('name')
                 })
 
         self.report({
             "results": results,
-            "last_update": self.__lastcommit()}
-        )
+            "is_uptodate": self.lastlocalcommit() == self.lastremotecommit()
+        })
 
     def summary(self, raw):
         taxonomies = []
