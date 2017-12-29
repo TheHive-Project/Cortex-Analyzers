5e09663ba84d42b56be789b26308ce88c0ec2f26
Nils Kuhnert <Nils.Kuhnert@posteo.de>
2017-12-29 14:09:00 +0100

    Minor, partly cosmetical, changes; Python3

--- analyzers/Bluecoat/categorization.py
+++ analyzers/Bluecoat/categorization.py
@@ -1,106 +1,107 @@
-#!/usr/bin/python
-#encoding: utf-8
+#!/usr/bin/python3
+import json
+import re
+import requests
+
 from cortexutils.analyzer import Analyzer
-import json, re, sys, requests
+
 
 class BluecoatAnalyzer(Analyzer):
 
-		def __init__(self):
-			Analyzer.__init__(self)
-			self.BC_url = "https://sitereview.bluecoat.com/"
-			self.BC_parameter_name = "url"
-			self.BC_sitereview = "sitereview.jsp"
-			self.BC_rest_page = "rest/categorization"
+    def __init__(self):
+        Analyzer.__init__(self)
+        self.BC_url = 'https://sitereview.bluecoat.com/'
+        self.BC_parameter_name = 'url'
+        self.BC_sitereview = 'sitereview.jsp'
+        self.BC_rest_page = 'rest/categorization'
 
-		"""
-			Extract desired fields through regex
-			- category
-			- id
-			- date
-		"""
-		def parse_answer(self, categorization, ratedate):
+    def parse_answer(self, categorization, ratedate):
+        """
+        Extract desired fields using RegEx
+        """
+        regex_category_id = r'catdesc\.jsp\?catnum=(\d+)'
+        regex_category = r'>([\w\s\/]+)<\/a>'
+        regex_date = r'Last Time Rated\/Reviewed:(.*)<img'
 
-			REGEX_NUM = r"catdesc\.jsp\?catnum=(\d+)"
-			REGEX_CATE = r">([\w\s\/]+)<\/a>"
-			REGEX_DATE = r"Last Time Rated\/Reviewed:(.*)<img"
+        if categorization != "":
+            result = {}
+            try:
+                result['category'] = re.findall(regex_category, categorization)[0]
+                result['id'] = re.findall(regex_category_id, categorization)[0]
+                result['date'] = re.findall(regex_date, ratedate)
+                if not result['date']:
+                    result['date'] = False
+                else:
+                    result['date'] = result['date'][0]
 
-			if categorization != "":
-				result = {}
-				try:
-					result["category"] = re.findall(REGEX_CATE, categorization)[0]
-					result["id"] = re.findall(REGEX_NUM, categorization)[0]
-					result["date"] = re.findall(REGEX_DATE, ratedate)
-					if result["date"] == []: result["date"] = False
-					else : result["date"] = result["date"][0]
+            except KeyError:
+                result = None
 
-				except Exception as e:
-					result = None
+            return result
+        else:
+            return None
 
-				return result
-			else : return None
+    def call_bluecoat_api(self, host):
+        """
+        Return JSON formatted data provided by Bluecoat REST API
+        """
+        session = requests.session()
+        try:
+            # First connexion in order to get a SESSION ID, used for the second request
+            session.get(self.BC_url + self.BC_sitereview)
+            BC_json_answer = session.post(self.BC_url + self.BC_rest_page, data={self.BC_parameter_name: host})
+            return json.loads(BC_json_answer.text)
+        except Exception as e:
+            self.error(e)
 
+    def url_to_domain(self, url):
+        """
+        Retrieve domain from url
+        """
+        regex_domain = r'(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)\/?'
+        try:
+            return re.findall(regex_domain, url)[0]
+        except:
+            return None
 
-		"""
-			return JSON formated data provided by Bluecoat REST API
-		"""
-		def callBlueCoatAPI(self, host):
-			session = requests.session()
-			try:
-				# First connexion in order to get a SESSION ID, used for the second request
-				session.get(self.BC_url + self.BC_sitereview)
-				BC_json_answer = session.post(self.BC_url + self.BC_rest_page, data={self.BC_parameter_name : host})
-				return json.loads(BC_json_answer.text)
-			except Exception as e:
-				self.error(str(e))
+    def summary(self, raw):
+        taxonomies = []
+        level = 'info'
+        namespace = 'BlueCoat'
+        predicate = 'Category'
+        value = '{}'.format(raw['category'])
 
+        if value == '\"Uncategorized\"':
+            level = 'suspicious'
 
-		"""
-			retrieve domain from url
-		"""
-		def url_to_domain(self, url):
-			REGEX_DOMAIN = r"(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)\/?"
-			try:
-				return  re.findall(REGEX_DOMAIN, url)[0]
-			except:
-				return None
+        taxonomies.append(self.build_taxonomy(level, namespace, predicate, value))
+        return {'taxonomies': taxonomies}
 
+    def run(self):
+        json_answer = None
+        if self.data_type == 'domain' or self.data_type == 'url' or self.data_type == 'fqdn':
+            if self.data_type == 'url':
+                domain = self.url_to_domain(self.getData())
+                if domain:
+                    json_answer = self.call_bluecoat_api(domain)
+                else:
+                    self.error('Domain not found')
 
-		def summary(self, raw):
-			taxonomies = []
-			level = "info"
-			namespace = "BC"
-			predicate = "Categorization"
-			value = "\""+raw["category"]+"\""
+            else:
+                json_answer = self.call_bluecoat_api(self.getData())
 
-			if value == "\"Uncategorized\"": level = "suspicious"
-
-			taxonomies.append(self.build_taxonomy(level, namespace, predicate, value))
-			return {"taxonomies" : taxonomies}
-
-
-		def run(self):
-
-			if self.data_type == "domain" or self.data_type == "url" or self.data_type == "fqdn":
-
-				if self.data_type == "url":
-					domain = self.url_to_domain(self.getData())
-					if domain != None: JSON_answer = self.callBlueCoatAPI(domain)
-					else: self.error("Domain not found")
-
-				else: JSON_answer = self.callBlueCoatAPI(self.getData())
-
-				if JSON_answer != None:
-					try:
-						result = self.parse_answer(JSON_answer["categorization"], JSON_answer["ratedate"])
-						result["hote"] = str(self.getData())
-						return self.report(result)
-					except Exception as e:
-						try:
-							return self.error("%s : %s" % (JSON_answer["errorType"], JSON_answer["error"]))
-						except Exception as b:
-							return self.error(str(b))
-			else:
-				return self.error("Invalid data type !")
+            if json_answer:
+                try:
+                    result = self.parse_answer(json_answer['categorization'], json_answer['ratedate'])
+                    result['host'] = self.getData()
+                    return self.report(result)
+                except Exception:
+                    try:
+                        return self.error('{} : {}'.format(json_answer['errorType'], json_answer['error']))
+                    except Exception as b:
+                        return self.error(b)
+        else:
+            return self.error('Invalid data type !')
 
 
 if __name__ == '__main__':
