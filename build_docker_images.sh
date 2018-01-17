3a77d48288f720ce8ce1a371983f1d29e09f2b35
Nils Kuhnert <Nils.Kuhnert@posteo.de>
2018-01-17 14:22:18 +0100

    WIP - should be buildable

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Vars
 VER=1.0.0
 
@@ -9,14 +10,15 @@ docker build -t cortex-base-python3:$VER contrib/docker/cortex-base-python3
 docker build -t cortex-base-python3:latest contrib/docker/cortex-base-python3
 
 echo "Building analyzer images..."
-for analyzer in `ls -1 analyzers/`; do
-  lower=`echo $analyzer | tr "[:upper:]" "[:lower:]"`
+for analyzer in `ls -1 analyzers/|grep -v File_Info`; do
+  lower=`echo ${analyzer} | tr "[:upper:]" "[:lower:]"`
   echo "Building cortex-analyzers-$lower"
-  OUTPUT="docker build -t cortex-analyzers-$lower analyzers/$analyzer"
+  OUTPUT=$(docker build -t cortex-analyzers-${lower} -f analyzers-docker/${analyzer}/Dockerfile .)
   if [ $? != 0 ]; then
-    /bin/echo -e "\e[91m$OUTPUT\e[0m"
-    /bin/echo -e "\e[7mError while building image for $analyzer.\e[0m"
+    echo -e "\e[91m$OUTPUT\e[0m"
+    echo -e "\e[100m\e[91mError while building image for $analyzer.\e[0m"
     exit 1
   fi
 done
+echo -e "\e[100m\e[92mSuccessfully built analyzer docker images. Check them via 'docker image ls'.\e[0m"
 
