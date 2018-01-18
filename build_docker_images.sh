b0a483e014c0630fc981b980ec4df49b9cc037e0
Nils Kuhnert <Nils.Kuhnert@posteo.de>
2018-01-18 09:36:34 +0100

    Script changes, added labels, yara image diet.

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
 # Vars
 VER=1.0.0
+PREFIX=$1
 
 # Docker base images
 echo "Building base images..."
-docker build -t cortex-base-python2:$VER contrib/docker/cortex-base-python2
-docker build -t cortex-base-python2:latest contrib/docker/cortex-base-python2
-docker build -t cortex-base-python3:$VER contrib/docker/cortex-base-python3
-docker build -t cortex-base-python3:latest contrib/docker/cortex-base-python3
+if [ ! ${PREFIX} = "" ]; then
+  docker build -t ${PREFIX}/cortex-base-python2:${VER} --build-arg VERSION=${VER} --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` --build-arg VCS_REF=`git rev-parse --short HEAD` contrib/docker/cortex-base-python2
+  docker build -t ${PREFIX}/cortex-base-python2:latest --build-arg VERSION=${VER} --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` --build-arg VCS_REF=`git rev-parse --short HEAD` contrib/docker/cortex-base-python2
+  docker build -t ${PREFIX}/cortex-base-python3:${VER} --build-arg VERSION=${VER} --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` --build-arg VCS_REF=`git rev-parse --short HEAD` contrib/docker/cortex-base-python3
+  docker build -t ${PREFIX}/cortex-base-python3:latest --build-arg VERSION=${VER} --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` --build-arg VCS_REF=`git rev-parse --short HEAD` contrib/docker/cortex-base-python3
+else
+  docker build -t cortex-base-python2:${VER} --build-arg VERSION=${VER} --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` --build-arg VCS_REF=`git rev-parse --short HEAD` contrib/docker/cortex-base-python2
+  docker build -t cortex-base-python2:latest --build-arg VERSION=${VER} --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` --build-arg VCS_REF=`git rev-parse --short HEAD` contrib/docker/cortex-base-python2
+  docker build -t cortex-base-python3:${VER} --build-arg VERSION=${VER} --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` --build-arg VCS_REF=`git rev-parse --short HEAD` contrib/docker/cortex-base-python3
+  docker build -t cortex-base-python3:latest --build-arg VERSION=${VER} --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` --build-arg VCS_REF=`git rev-parse --short HEAD` contrib/docker/cortex-base-python3
+fi
 
 echo "Building analyzer images..."
 for analyzer in `ls -1 analyzers/|grep -v File_Info`; do
   lower=`echo ${analyzer} | tr "[:upper:]" "[:lower:]"`
   echo "Building cortex-analyzers-$lower"
-  OUTPUT=$(docker build -t cortex-analyzers-${lower} -f analyzers-docker/${analyzer}/Dockerfile .)
+  if [ ! ${PREFIX} = "" ]; then
+    OUTPUT=$(docker build -t ${PREFIX}/cortex-analyzers-${lower} -f analyzers-docker/${analyzer}/Dockerfile .)
+  else
+    OUTPUT=$(docker build -t cortex-analyzers-${lower} -f analyzers-docker/${analyzer}/Dockerfile .)
+  fi
   if [ $? != 0 ]; then
     echo -e "\e[91m$OUTPUT\e[0m"
     echo -e "\e[100m\e[91mError while building image for $analyzer.\e[0m"
