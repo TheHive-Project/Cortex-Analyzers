0a66ee8efe751a7cd06a410d6eeccfeed3d5117c
Nils Kuhnert <Nils.Kuhnert@posteo.de>
2018-03-21 15:20:08 +0100

    Added test for previous commit and bumped version.

--- contrib/tests/test_suite_integration.py
+++ contrib/tests/test_suite_integration.py
@@ -34,3 +34,25 @@ class AnalyzerExtractorOutputTest(unittest.TestCase):
         self.assertNotIn(self.analyzer.get_data(), output)
         self.assertEqual(json_output['artifacts'][0]['value'], '1.2.3.4')
         self.assertEqual(json_output['artifacts'][0]['type'], 'ip')
+
+class AnalyzerExtractorNoResultTest(unittest.TestCase):
+    def setUp(self):
+        sys.stdin = StringIO(json.dumps({
+            "data": "8.8.8.8",
+            "dataType": "ip"
+        }))
+        sys.stdout = StringIO()
+        self.analyzer = Analyzer()
+
+    def test_output(self):
+        # Run report method
+        self.analyzer.report({
+            'message': '8.8.8.8 was not found in database.'
+        })
+
+        # Grab the output
+        output = self.analyzer.fpoutput.getvalue().strip()
+        json_output = json.loads(output)
+
+        # Check for empty artifact list
+        self.assertEqual(json_output['artifacts'], [], 'Artifact list should be empty.')
