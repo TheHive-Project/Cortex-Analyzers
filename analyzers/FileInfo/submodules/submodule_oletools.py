7d5803e70f6636517a07f092b4e0f110d6c64ab1
Nils Kuhnert <Nils.Kuhnert@cert-bund.de>
2018-03-29 11:27:59 +0200

    Added DDE scan and OleID

--- analyzers/FileInfo/submodules/submodule_oletools.py
+++ analyzers/FileInfo/submodules/submodule_oletools.py
@@ -1,6 +1,8 @@
 """FileInfo oletools submodule; WIP"""
 from .submodule_base import SubmoduleBaseclass
+from oletools.oleid import OleID
 from oletools.olevba3 import VBA_Parser, VBA_Scanner, ProcessingError
+from oletools.msodde import process_file
 
 
 class OLEToolsSubmodule(SubmoduleBaseclass):
@@ -8,7 +10,7 @@ class OLEToolsSubmodule(SubmoduleBaseclass):
 
     def __init__(self):
         SubmoduleBaseclass.__init__(self)
-        self.name = 'OLETools Submodule'
+        self.name = 'Oletools Submodule'
         self.fileextension = None
 
     def check_file(self, **kwargs):
@@ -30,17 +32,29 @@ class OLEToolsSubmodule(SubmoduleBaseclass):
 
     def analyze_file(self, path):
         # Run the analyze functions
+        self.analyze_oleid(path)
         self.analyze_vba(path)
+        self.analyze_dde(path)
 
         return self.results
+
+    def analyze_oleid(self, path):
+        indicators = OleID(path).check()
+        results = {}
+
+        for indicator in indicators:
+            if indicator.id == 'appname':
+                continue
+            results.update({indicator.name: indicator.value})
+        self.add_result_subsection('Oletools OleID Results', results)
 
     def analyze_vba(self, path):
         """Analyze a given sample for malicios vba."""
         try:
-            parsed = VBA_Parser(path)
+            parser = VBA_Parser(path)
 
-            if parsed.detect_vba_macros():
-                for idx, (filename, stream_path, vba_filename, vba_code) in enumerate(parsed.extract_all_macros()):
+            if parser.detect_vba_macros():
+                for idx, (filename, stream_path, vba_filename, vba_code) in enumerate(parser.extract_all_macros()):
                     # Decode strings often produces errors
                     scanner = VBA_Scanner(vba_code)
                     scan_results_to_report = []
@@ -66,6 +80,12 @@ class OLEToolsSubmodule(SubmoduleBaseclass):
                     )
 
         except TypeError:
-            self.add_result_subsection('OLEVBA Analysis failed', 'Analysis failed due to an filetype error.'
+            self.add_result_subsection('Oletools VBA Analysis failed', 'Analysis failed due to an filetype error.'
                                                                  'The file does not seem to be a valid MS-Office file.')
 
+    def analyze_dde(self, path):
+        results = process_file(path)
+        if len(results) > 0:
+            self.add_result_subsection('Oletools DDE Analysis', {'DDEUrl': results})
+        else:
+            self.add_result_subsection('Oletools DDE Analysis', {'Info': 'No URLs found.'})
