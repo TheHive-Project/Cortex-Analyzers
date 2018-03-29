597caa4f9c7c4a82794bb864ea4e7e8ac29915c4
Nils Kuhnert <Nils.Kuhnert@cert-bund.de>
2018-03-29 10:20:32 +0200

    Added some olevba functionality.

--- analyzers/FileInfo/submodules/submodule_oletools.py
+++ analyzers/FileInfo/submodules/submodule_oletools.py
@@ -1,5 +1,6 @@
 """FileInfo oletools submodule; WIP"""
 from .submodule_base import SubmoduleBaseclass
+from oletools.olevba3 import VBA_Parser, VBA_Scanner, ProcessingError
 
 
 class OLEToolsSubmodule(SubmoduleBaseclass):
@@ -8,11 +9,13 @@ class OLEToolsSubmodule(SubmoduleBaseclass):
     def __init__(self):
         SubmoduleBaseclass.__init__(self)
         self.name = 'OLETools Submodule'
+        self.fileextension = None
 
     def check_file(self, **kwargs):
         """Oletools accepts MS office documents."""
         try:
-            if kwargs.get('filename').rsplit('.', 1)[1] in [
+            self.fileextension = kwargs.get('filename').rsplit('.', 1)[1]
+            if self.fileextension in [
                 'doc',
                 'docx',
                 'xls',
@@ -24,3 +27,42 @@ class OLEToolsSubmodule(SubmoduleBaseclass):
         except KeyError:
             return False
         return False
+
+    def analyze_file(self, path):
+        pass
+
+    def analyze_vba(self, path):
+        """Analyze a given sample for malicios vba."""
+        try:
+            parsed = VBA_Parser(path)
+
+            if parsed.detect_vba_macros():
+                for idx, (filename, stream_path, vba_filename, vba_code) in enumerate(parsed.extract_all_macros()):
+                    # Decode strings often produces errors
+                    scanner = VBA_Scanner(vba_code)
+                    scan_results_to_report = []
+                    try:
+                        scan_results = scanner.scan(include_decoded_strings=True)
+                    except ProcessingError:
+                        scan_results = scanner.scan(include_decoded_strings=False)
+
+                    for type, keyword, description in scan_results:
+                        scan_results_to_report.append({
+                            'type': type,
+                            'keyword': keyword,
+                            'description': description
+                        })
+
+                    self.add_result_subsection(
+                        'OLE stream: {}'.format(stream_path),
+                        {
+                            'olevba_filename': vba_filename,
+                            'olevba_code': vba_code,
+                            'olevba_results': scan_results_to_report
+                        }
+                    )
+
+        except TypeError:
+            self.add_result_subsection('OLEVBA Analysis failed', 'Analysis failed due to an filetype error.'
+                                                                 'The file does not seem to be a valid MS-Office file.')
+
