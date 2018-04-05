c2f9cf9df3a079432f8bc2ca92195ca702ab4fab
srilumpa <marcandre.doll@gmail.com>
2018-04-05 12:15:02 +0200

    Fix some data format

--- analyzers/StopForumSpam/stopforumspam_client.py
+++ analyzers/StopForumSpam/stopforumspam_client.py
@@ -3,7 +3,7 @@ import requests
 
 class StopforumspamClient:
 
-    _type_conversion = {'ip':'ip', 'mail':'email'}
+    _type_conversion = {'ip': 'ip', 'mail': 'email'}
 
     def __init__(self):
         self.client = requests.Session()
@@ -15,15 +15,22 @@ class StopforumspamClient:
             StopforumspamClient._type_conversion[type]: data
         }
 
+    def data_conversion(self, data):
+        if 'appears' in data:
+            data['appears'] = (data['appears'] == 1)
+        if 'torexit' in data:
+            data['torexit'] = (data['torexit'] == 1)
+        return data
+
     def get_data(self, datatype, data):
-        result = {}
+        result = []
         params = StopforumspamClient._set_payload(datatype, data)
         response = self.client.get('https://api.stopforumspam.org/api', params=params)
         response.raise_for_status()
         report = response.json()
-        print(report)
         if report['success']:
-            result = report[StopforumspamClient._type_conversion[datatype]]
+            data = report[StopforumspamClient._type_conversion[datatype]]
+            result.append(self.data_conversion(data))
         else:
             pass
         return result
