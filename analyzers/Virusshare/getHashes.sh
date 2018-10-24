1c13bfa26d4382855bed5c3e3073e4596c869e5f
garanews <puntogtg@tiscali.it>
2018-10-24 14:06:12 +0000

    fix: cut: the delimiter must be a single character
    fix the script

--- analyzers/Virusshare/getHashes.sh
+++ analyzers/Virusshare/getHashes.sh
@@ -22,7 +22,7 @@ fi
 
 cd $1
 for u in `curl https://virusshare.com/hashes.4n6 | grep -E "VirusShare_[0-9]{5}\.md5" | c\
-ut -d -f2 | cut -d\/ -f2`
+ut -d\" -f2 | cut -d\/ -f2`
 do
     echo $u
     if [ -e $1/$u ]; then
