0cebd39733090a28b4726dcc00e8b5eb0735096b
Jérôme Leonard <jerome@thehive-project.org>
2019-09-20 17:24:08 +0200

    #533 ehance report and add hashes of decrypted document

--- analyzers/FileInfo/submodules/submodule_oletools.py
+++ analyzers/FileInfo/submodules/submodule_oletools.py
@@ -41,7 +41,24 @@ class OLEToolsSubmodule(SubmoduleBaseclass):
 
     def analyze_file(self, path):
         # Run the analyze functions
-        self.encypted = is_encrypted(path)
+        self.encrypted = {'encrypted': is_encrypted(path)}
+        if is_encrypted(path):
+            import io, hashlib
+
+            with io.open(path, 'rb') as fh:
+                buf = fh.read()
+                md5 = hashlib.md5()
+                md5.update(buf)
+                sha1 = hashlib.sha1()
+                sha1.update(buf)
+                sha256 = hashlib.sha256()
+                sha256.update(buf)
+
+            self.encrypted = {'encrypted': is_encrypted(path),
+                             'sha256': sha256.hexdigest(),
+                             'sha1': sha1.hexdigest(),
+                             'md5': md5.hexdigest()}
+
         self.analyze_vba(path)
         self.analyze_dde(path)
 
@@ -98,8 +115,7 @@ class OLEToolsSubmodule(SubmoduleBaseclass):
         self.summary['taxonomies'] = taxonomies
         self.summary['Olevba'] = olevba_version
         self.summary['Msodde'] = msodde_version
-        self.summary['encrypted_file'] = self.encypted
-
+        self.summary['encrypted_file'] = self.encrypted
         return self.summary
 
     def analyze_vba(self, path):
