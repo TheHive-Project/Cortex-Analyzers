2bf57c29fae8eb601c2bfc49681a6b53f90bd8a9
Stephen O'Leary <stephen.oleary@sophos.com>
2019-11-12 14:44:46 +0000

    moving client call to init

--- analyzers/SophosIntelix/intelix_analyzer.py
+++ analyzers/SophosIntelix/intelix_analyzer.py
@@ -12,14 +12,13 @@ class SophosIntelixAnalyzer(Analyzer):
         self.clientId = self.get_param('config.clientID', None, 'ClientId is Missing')
         self.clientSecret = self.get_param('config.clientSecret', None, 'Client Secret is Missing')
         self.polling_interval = self.get_param('config.polling_interval', 60)
-
-    def run(self):
         try:
-            self.ic = intelix.client(self.clientId,self.clientSecret)
+            self.ic = intelix.client(self.clientId, self.clientSecret)
         except Exception as e:
             error = str(e)
             self.error('Error: {}'.format(error))
-            
+
+    def run(self):
         if self.service == 'get':
             if self.data_type == 'hash':
                 try:
