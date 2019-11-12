fabee58cd9e1089cbb1a642f0d0fcfbc30bcdfec
Stephen O'Leary <stephen.oleary@sophos.com>
2019-11-12 17:56:17 +0000

    added service files for static and dynamic file analysis submissions

---
+++ analyzers/SophosIntelix/SophosIntelix_Submit_Dynamic.py
@@ -0,0 +1,38 @@
+{
+  "name": "SophosIntelix_Submit_Dynamic",
+  "version": "0.1",
+  "author": "SOL",
+  "url": "https://github.com/TheHive-Project/Cortex-Analyzers",
+  "license": "AGPL-V3",
+  "description": "Submits a file to Sophos Intelix for Dynamic Analysis",
+  "dataTypeList": ["file"],
+  "command": "SophosIntelix/intelix_analyzer.py",
+  "baseConfig": "SophosIntelix",
+  "config": {
+    "service": "submit_dynamic"
+  },
+  "configurationItems": [
+    {
+      "name": "clientID",
+      "description": "Client ID for Sophos Labs Intelix",
+      "type": "string",
+      "multi": false,
+      "required": true
+    },
+    {
+      "name": "clientSecret",
+      "description": "Client Secret for Sophos Labs Intelix",
+      "type": "string",
+      "multi": false,
+      "required": true
+    },
+    {
+      "name": "polling_interval",
+      "description": "Define time interval between two requests attempts for the report",
+      "type": "number",
+      "multi": false,
+      "required": false,
+      "defaultValue": 60
+    }
+  ]
+}
