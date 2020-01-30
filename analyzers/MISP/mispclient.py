f19d4494a2659ba691a396ad7517db38bb0c74c2
3c7 <Nils.Kuhnert@cert-bund.de>
2020-01-30 15:45:45 +0100

    [#602] Use ExpandedPyMISP in case someone uses legacy pymisp version

--- analyzers/MISP/mispclient.py
+++ analyzers/MISP/mispclient.py
@@ -56,7 +56,7 @@ class MISPClient:
                     raise CertificateNotFoundError('Certificate not found under {}.'.format(ssl))
                 elif isinstance(ssl, bool):
                     verify = ssl
-                self.misp_connections.append(pymisp.PyMISP(url=server,
+                self.misp_connections.append(pymisp.ExpandedPyMISP(url=server,
                                                            key=key[idx],
                                                            ssl=verify,
                                                            proxies=proxies))
