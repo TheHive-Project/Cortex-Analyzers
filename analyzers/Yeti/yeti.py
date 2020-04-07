9153de44473c7c44cdb0e2cf3bc96e7ec070ce59
Davide Arcuri <dadokkio@gmail.com>
2020-04-07 11:46:56 +0200

    yeti fix issues

--- analyzers/Yeti/yeti.py
+++ analyzers/Yeti/yeti.py
@@ -10,6 +10,7 @@ class YetiAnalyzer(Analyzer):
         Analyzer.__init__(self)
         self.url = self.get_param('config.url', None, 'Missing URL for Yeti API')
         self.api_key = self.get_param('config.api_key')
+        self.verify_ssl = self.get_param("config.verify_ssl", True, None)
 
     def summary(self, raw):
         count = len(raw.get('findings', []))
@@ -26,14 +27,11 @@ class YetiAnalyzer(Analyzer):
         return result
 
     def run(self):
-        api = pyeti.YetiApi("{}/api/".format(self.url), api_key=self.api_key)
+        api = pyeti.YetiApi("{}/api/".format(self.url), api_key=self.api_key, verify_ssl=self.verify_ssl)
         data = self.get_data()
 
         try:
             result = api.observable_search(value=data)
-
-            if not result:
-                self.error('Service unavailable, please check if Yeti server is running')
 
             self.report({
                 'findings': result
