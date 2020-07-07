73a277642747ebe2293d8b0c2324f8ff3f5ceb35
Davide Arcuri <darcuri@malcert.com>
2020-07-07 14:01:59 +0200

    add templates and small fixes

--- analyzers/DomainMailSPFDMARC/domainMailSPFDMARC.py
+++ analyzers/DomainMailSPFDMARC/domainMailSPFDMARC.py
@@ -6,44 +6,43 @@ from cortexutils.analyzer import Analyzer
 import checkdmarc
 
 class DomainMailSPFDMARC(Analyzer):
-        def __init__(self):
-                Analyzer.__init__(self)
-                self.name = "DomainMailSPFDMARC"
-        def summary(self, raw):
-                taxonomies = []
-                level = "malicious"
-                level_s = "suspicious"
-                level_sa = "safe"
-                namespace = "DomainMailSPF_DMARC"
-                predicate = "tag"
+    def __init__(self):
+        Analyzer.__init__(self)
+        self.name = "DomainMailSPFDMARC"
 
-                if 'error' in raw['DomainMailSPFDMARC_info']['DomainMailSPFDMARC']['dmarc']:
-                        if 'error' in raw['DomainMailSPFDMARC_info']['DomainMailSPFDMARC']['spf']:
-                                taxonomies.append(self.build_taxonomy(level, namespace,"DMARC","no"))
-                                taxonomies.append(self.build_taxonomy(level, namespace,"SPF","no"))
-                        else:
-                                taxonomies.append(self.build_taxonomy(level_sa, namespace,"SPF","yes"))
-                                taxonomies.append(self.build_taxonomy(level_s, namespace,"DMARC","no"))
-                else:
-                        if 'error' in raw['DomainMailSPFDMARC_info']['DomainMailSPFDMARC']['spf']:
-                                taxonomies.append(self.build_taxonomy(level_s, namespace,"SPF","no"))
-                                taxonomies.append(self.build_taxonomy(level_sa, namespace,"DMARC","yes"))
-                        else:
-                                taxonomies.append(self.build_taxonomy(level_sa, namespace,"SPF","yes"))
-                                taxonomies.append(self.build_taxonomy(level_sa, namespace,"DMARC","yes"))
+    def summary(self, raw):
+        taxonomies = []
+        namespace = "DomainMailSPF_DMARC"
 
-                return {'taxonomies': taxonomies}
-        def get_info(self, data):
-                try:
-                        result = checkdmarc.check_domains(data.split()) 
-                except ValueError:
-                        print("Explotioooooooo")
-                return {"DomainMailSPFDMARC": dict(result)}
+        if 'error' in raw['DomainMailSPFDMARC']['dmarc']:
+            if 'error' in raw['DomainMailSPFDMARC']['spf']:
+                taxonomies.append(self.build_taxonomy("malicious", namespace,"DMARC","no"))
+                taxonomies.append(self.build_taxonomy("malicious", namespace,"SPF","no"))
+            else:
+                taxonomies.append(self.build_taxonomy("safe", namespace,"SPF","yes"))
+                taxonomies.append(self.build_taxonomy("suspicious", namespace,"DMARC","no"))
+        else:
+            if 'error' in raw['DomainMailSPFDMARC']['spf']:
+                taxonomies.append(self.build_taxonomy("suspicious", namespace,"SPF","no"))
+                taxonomies.append(self.build_taxonomy("safe", namespace,"DMARC","yes"))
+            else:
+                taxonomies.append(self.build_taxonomy("safe", namespace,"SPF","yes"))
+                taxonomies.append(self.build_taxonomy("safe", namespace,"DMARC","yes"))
+        
+        return {'taxonomies': taxonomies}
+        
+    def get_info(self, data):
+        try:
+            result = checkdmarc.check_domains(data.split()) 
+        except Exception as e :
+            self.error(e)
+        return {"DomainMailSPFDMARC": dict(result)}
 
-        def run(self):
-                if self.data_type == 'domain' or self.data_type == 'fqdn':
-                        data = self.get_data()
-                        self.report({"DomainMailSPFDMARC_info": self.get_info(data)})
+    def run(self):
+        if self.data_type == 'domain' or self.data_type == 'fqdn':
+            self.report(self.get_info(self.get_data()))
+        else:
+            self.error('Data type not supported. Please use this analyzer with data types domain or fqdn.')
 
 if __name__ == '__main__':
-        DomainMailSPFDMARC().run()
+    DomainMailSPFDMARC().run()
