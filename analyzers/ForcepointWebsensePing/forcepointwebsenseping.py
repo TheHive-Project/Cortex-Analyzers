d35cd3e4e458a0027c5d457b7d35be9d2b6cd4a5
dadokkio <dadokkio@gmail.com>
2020-07-10 18:48:50 +0200

    first analyzer code, untested and with categories taken from fortiguard

---
+++ analyzers/ForcepointWebsensePing/forcepointwebsenseping.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+
+import subprocess
+from cortexutils.analyzer import Analyzer
+
+
+class WebsensePingAnalyzer(Analyzer):
+
+    def __init__(self):
+        Analyzer.__init__(self)
+        self.hostname = self.get_param('config.hostname', None)
+        self.timeout = self.get_param('config.timeout', None)
+        self.path = self.get_param('config.path', None)
+
+    def summary(self, raw):
+        taxonomies = []
+        if raw.get('Categories', None):
+            value = raw['Categories']
+            if r in self.get_param('config.malicious_categories', []):
+                level = "malicious"
+            elif r in self.get_param('config.suspicious_categories', []):
+                level = "suspicious"
+            else:
+                level = "info"
+            taxonomies.append(self.build_taxonomy(level, "Fortinet", "WebsensePing", value))
+        return {"taxonomies": taxonomies}
+
+    def run(self):
+        Analyzer.run(self)
+
+        if self.data_type in ("url", "ip", "domain","fqdn"):
+            data = self.get_param('data', None, 'Data is missing')
+            command = "export LD_LIBRARY_PATH={} && {}/WebsensePing -m 25 -url {} -s {} -t {}".format(self.path, self.path, data, self.hostname, self.timeout)
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output = [line.decode('utf8') for line in process.stdout]
+            report = {k.strip():v.strip() for k,v in [x.split("=") for x in output if x.find("=") != -1]}
+            self.report(report)
+        else:
+            self.error('Invalid data type')
+
+
+if __name__ == '__main__':
+    WebsensePingAnalyzer().run()
