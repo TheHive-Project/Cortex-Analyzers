1b1f92490c9edd9b23a965713f702b6a7a83bcda
garanews <puntogtg@tiscali.it>
2020-07-10 23:49:27 +0200

    added safe categories

--- analyzers/ForcepointWebsensePing/forcepointwebsenseping.py
+++ analyzers/ForcepointWebsensePing/forcepointwebsenseping.py
@@ -14,18 +14,20 @@ class WebsensePingAnalyzer(Analyzer):
         self.timeout = self.get_param('config.timeout', None)
         self.path = self.get_param('config.path', None)
 
-    def summary(self, raw):
-        taxonomies = []
-        if raw.get('Categories', None):
-            value = raw['Categories']
+def summary(self, raw):
+    taxonomies = []
+    if raw.get('Categories', None):
+        value = raw['Categories']
         if value in self.get_param('config.malicious_categories', []):
             level = "malicious"
         elif value in self.get_param('config.suspicious_categories', []):
             level = "suspicious"
+        elif value in self.get_param('config.safe_categories', []):
+            level = "safe"
         else:
             level = "info"
-            taxonomies.append(self.build_taxonomy(level, "Forcepoint", "WebsensePing", value))
-        return {"taxonomies": taxonomies}
+        taxonomies.append(self.build_taxonomy(level, "Forcepoint", "WebsensePing", value))
+    return {"taxonomies": taxonomies}
 
     def run(self):
         Analyzer.run(self)
