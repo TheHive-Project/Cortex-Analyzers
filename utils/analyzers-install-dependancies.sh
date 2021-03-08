2e0ac49f082748304dd0b6c6fc8c75697c72892a
adammchugh <34958726+adammchugh@users.noreply.github.com>
2021-03-08 14:53:33 +1030

    Bash file to install and update dependancies from a single command rather than manually/semi-manually installing requirements.

--- utils/analyzers-install-dependancies.sh
+++ utils/analyzers-install-dependancies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-for I in $(find Cortex-Analyzers -name 'requirements.txt'); do sudo -H pip2 install -r $I; done
-for I in $(find Cortex-Analyzers -name 'requirements.txt'); do sudo -H pip3 install -r $I; done
+for I in $(find AdamMcHugh-Cortex-Analyzers -name 'requirements.txt'); do sudo -H pip2 install -r $I; done
+for I in $(find AdamMcHugh-Cortex-Analyzers -name 'requirements.txt'); do sudo -H pip3 install -r $I; done
