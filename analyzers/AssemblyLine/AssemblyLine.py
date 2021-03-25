f285d58553b51cd37213310c541b8d7cb6876795
Adam McHugh <adammchugh83@gmail.com>
2021-03-25 20:04:34 +1030

    Rewrite of Analyzer structure and simplification of services.

--- analyzers/AssemblyLine/AssemblyLine.py
+++ analyzers/AssemblyLine/AssemblyLine.py
@@ -77,7 +77,7 @@ class AssemblyLineAnalyzer(Analyzer):
 
     def RetrieveAnalysis(self):
         al_client = get_client(self.assemblyline_host, apikey=(self.assemblyline_user, self.assemblyline_key), verify=False)
-        submissions = al_client.search.stream.submission("files.sha256:" + self.hash):
+        submissions = al_client.search.stream.submission("files.sha256:" + self.hash)
         return json.dumps(submissions)
         for submission in al_client.search.stream.submission("files.sha256:" + self.hash):
             full_submission = al_client.submission(submission['id'])
