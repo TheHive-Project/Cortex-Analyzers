4e1b4dfc0f1ba450fae20abe87bc3f0f1303197e
Adam McHugh <adammchugh83@gmail.com>
2021-03-25 21:07:39 +1030

    Assemblyline Cortex Analyzer

--- utils/analyzers-install-dependancies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-for I in $(find Cortex-Analyzers -name 'requirements.txt'); do sudo -H pip2 install -r $I; done
-for I in $(find Cortex-Analyzers -name 'requirements.txt'); do sudo -H pip3 install -r $I; done
