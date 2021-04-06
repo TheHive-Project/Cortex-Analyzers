3844203324f34f97052ca8cdd8bd8a416dc6ca55
Brad Chiappetta <brad@greynoise.io>
2021-04-06 14:19:45 -0400

    first pass

--- analyzers/GreyNoise/greynoisev3.py
+++ analyzers/GreyNoise/greynoisev3.py
@@ -16,11 +16,20 @@ class GreyNoiseAnalyzer(Analyzer):
 
         if self.data_type == "ip":
             api_key = self.get_param("config.key", None)
-            api_client = GreyNoise(
-                api_key=api_key,
-                timeout=30,
-                integration_name="greynoise-cortex-analyzer-v3.0",
-            )
+            api_type = self.get_param("config.api_type", None)
+            if api_type and api_type.lower() == "community":
+                api_client = GreyNoise(
+                    api_key=api_key,
+                    timeout=30,
+                    integration_name="greynoise-cortex-analyzer-v3.1",
+                    offering="community"
+                )
+            else:
+                api_client = GreyNoise(
+                    api_key=api_key,
+                    timeout=30,
+                    integration_name="greynoise-cortex-analyzer-v3.1",
+                )
             try:
                 self.report(api_client.ip(self.get_data()))
             except Exception as e:
@@ -98,6 +107,8 @@ class GreyNoiseAnalyzer(Analyzer):
             taxonomies = []
 
             seen = raw.get("seen", False)
+            noise = raw.get("noise", False)
+            riot = raw.get("riot", False)
             if seen:
                 tag_count = len(raw.get("tags", []))
                 classification = raw.get("classification", "unknown")
@@ -122,12 +133,35 @@ class GreyNoiseAnalyzer(Analyzer):
                 taxonomies.append(
                     self.build_taxonomy(t2_level, t2_namespace, t2_predicate, t2_value)
                 )
+            elif noise or riot:
+                classification = raw.get("classification", "unknown")
+                name = raw.get("name")
+                t1_level = classification_level_map.get(classification)(None)
+                t1_namespace = "GreyNoise"
+                t1_predicate = classification
+                t1_value = (
+                    "Benign Service" if riot else "Internet Noise"
+                )
+                # print('{}:{} = {} ({})'.format(t1_namespace, t1_predicate, t1_value))
+                taxonomies.append(
+                    self.build_taxonomy(t1_level, t1_namespace, t1_predicate, t1_value)
+                )
+                t2_level = classification_level_map.get(classification)(None)
+                t2_namespace = "GreyNoise"
+                t2_predicate = (
+                    name if classification == "benign" else "classification"
+                )
+                t2_value = name if classification == "benign" else classification
+                # print('{}:{} = {} ({})'.format(t2_namespace, t2_predicate, t2_value, t2_level))
+                taxonomies.append(
+                    self.build_taxonomy(t2_level, t2_namespace, t2_predicate, t2_value)
+                )
             else:
                 taxonomies.append(
                     self.build_taxonomy(
                         classification_level_map.get("unknown")(None),
                         "GreyNoise",
-                        "Seen last 60 days",
+                        "IP not observed scanning the internet in the last 90 days",
                         False,
                     )
                 )
