066fb38ba901ab5f6bcd7e2432463440fc0528ca
Keijo Korte <git@kvak.net>
2021-10-29 16:36:04 +0300

    Working version

--- responders/Netcraft/Netcraft.py
+++ responders/Netcraft/Netcraft.py
@@ -3,64 +3,56 @@
 
 from cortexutils.responder import Responder
 import requests
-from datetime import datetime
 
 
-class UmbrellaBlacklister(Responder):
+class NetcraftReporter(Responder):
     def __init__(self):
         Responder.__init__(self)
-        self.integration_url = self.get_param(
-            'config.integration_url', None, "Integration URL Missing")
+        self.scheme = "https"
+        self.api_key = self.get_param(
+            'config.api_key', None, "API-key Missing")
+        self.takedown_url = self.get_param(
+            'config.takedown_url', None, "Takedown URL Missing")
+        self.observable_type = self.get_param('data.dataType', None, "Data type is empty")
+        self.observable_description = self.get_param('data.message', None, "Description is empty")
 
     def run(self):
         Responder.run(self)
+        try:
+            supported_observables = ["domain", "url", "fqdn"]
+            if self.observable_type in supported_observables:
+                if self.observable_type == "domain" or self.observable_type == "fqdn":
+                    domain = self.get_param('data.data', None, 'No artifacts available')
+                    takedown = "{}://{}".format(self.scheme, domain)
+                elif self.observable_type == "url":
+                    takedown = self.get_param('data.data')
 
-        data_type = self.get_param('data.dataType')
-        ioc_types = ["domain", "url", "fqdn"]
-        if data_type in ioc_types:
+                headers = {
+                    "Authorization": "Bearer {0}".format(self.api_key),
+                    'user-agent': 'Netcraft-Cortex-Responder'
+                }
+                payload = {
+                    "attack": takedown,
+                    "comment": "Automated takedown via Cortex"
+                }
 
-            if data_type == "domain" or data_type == "fqdn":
-                domain = self.get_param(
-                    'data.data', None, 'No artifacts available')
-
-                dstUrl = "http://" + domain
-
-            elif data_type == "url":
-                dstUrl = self.get_param(
-                    'data.data', None, 'No artifacts available')
-
-                domain = dstUrl.split('/')[2]
-
-            date = datetime.now().strftime("%Y-%m-%dT%XZ")
-
-            headers = {
-                'user-agent': 'UmbrellaBlacklister-Cortex-Responder',
-                'Content-Type': 'application/json'
-            }
-
-            payload = {
-                "alertTime": date,
-                "deviceId": "cortex_thehive",
-                "deviceVersion": "2.4.81",
-                "dstDomain": domain,
-                "dstUrl": dstUrl,
-                "eventTime": date,
-                "protocolVersion": "1.0a",
-                "providerName": "Security Platform"
-            }
-
-            r = requests.post(self.integration_url,
-                              json=payload, headers=headers)
-            if r.status_code == 200 | 202:
-                self.report({'message': 'Blacklisted in Umbrella.'})
+                response = requests.post(self.takedown_url, data=payload, headers=headers)
+                if response.status_code == 200:
+                    self.report({'message': 'Takedown sent ot Netcraft. Message: {}'.format(response.text)})
+                elif response.status_code == 401:
+                    self.report({'message': 'Failed authentication. Check API-Key Message: {}'.format(response.text)})
+                else:
+                    self.error('Failed to submit takedown request. Error code: {}. Error message: {}'
+                               .format(response.status_code, response.text))
             else:
-                self.error('Failed to add to blacklist.')
-        else:
-            self.error('Incorrect dataType. "Domain", "FQDN", or "URL" expected.')
+                self.error('Incorrect dataType. "Domain", "FQDN", or "URL" expected.')
+
+        except requests.exceptions.RequestException as e:
+            self.error(str(e))
 
     def operations(self, raw):
-        return [self.build_operation('AddTagToArtifact', tag='Umbrella:blocked')]
+        return [self.build_operation('AddTagToArtifact', tag='Netcraft:takedown')]
 
 
 if __name__ == '__main__':
-    UmbrellaBlacklister().run()
+    NetcraftReporter().run()
