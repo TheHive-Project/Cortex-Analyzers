41b960e852e8ee1db4230831e1e2ab8ea8513965
Mikael <info@prowling.nu>
2022-01-13 12:28:19 +0100

    1.1
    Updated to support more data and more hash types

--- analyzers/CIRCLHashlookup/circlhashlookup_analyzer.py
+++ analyzers/CIRCLHashlookup/circlhashlookup_analyzer.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 import requests
+import re
 from cortexutils.analyzer import Analyzer
 
 class CIRCLHashlookupAnalyzer(Analyzer):
@@ -13,7 +14,7 @@ class CIRCLHashlookupAnalyzer(Analyzer):
          taxonomies = []
          namespace = "CIRCLHashlookup"
 
-         if raw.get('CRC32'):
+         if raw.get('hashlookup:trust'):
             verdict = "safe"
             result = "known"
          else:
@@ -32,13 +33,17 @@ class CIRCLHashlookupAnalyzer(Analyzer):
     def run(self):
             if self.data_type == 'hash':
                 data = self.get_param('data', None, 'Data is missing')
-
                 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
                 session = requests.Session()
-                if len(data) == 32:
+                # Type of hash
+                if re.search(r'^[0-9a-f]{32}$', data, re.IGNORECASE):
                     s = session.get(self.url + '/lookup/md5/' + data, headers=headers)
-                elif len(data) == 40:
+                elif re.search(r'^[a-f0-9]{40}$', data, re.IGNORECASE):
                     s = session.get(self.url + '/lookup/sha1/' + data, headers=headers)
+                elif re.search(r'^[a-f0-9]{64}$', data, re.IGNORECASE):
+                    s = session.get(self.url + '/lookup/sha256/' + data, headers=headers)
+                elif re.search(r'^[a-f0-9]{128}$', data, re.IGNORECASE):
+                    s = session.get(self.url + '/lookup/sha512/' + data, headers=headers)
                 else:
                     self.error('Unsupported hash type')
 
