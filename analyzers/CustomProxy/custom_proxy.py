048cc0917b2da43c4ecb5872425e8d4934594aba
CTIMarc <marc.nebout@sekoia.io>
2023-11-16 15:42:41 +0100

    fix path parameter

--- analyzers/CustomProxy/custom_proxy.py
+++ analyzers/CustomProxy/custom_proxy.py
@@ -54,6 +54,7 @@ class CurstomProxy(Analyzer):
         try:
             method = self.get_param('parameters.method', default='GET')
             module = self.get_param('parameters.module', default="get-tor")
+            module = self.get_param('parameters.path', default="")
             url = self.get_param('data', None, 'Data param is missing')
             headers = self.get_param('parameters.headers', default={})
             post_data = self.get_param('parameters.post_data', default={})
