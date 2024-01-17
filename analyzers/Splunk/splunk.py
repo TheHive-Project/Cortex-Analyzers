b658dfaed8c04e59c34928b3dc37f9f8a1545397
cyberpescadito <32546144+cyberpescadito@users.noreply.github.com>
2024-01-18 00:08:33 +0100

    Update splunk.py

--- analyzers/Splunk/splunk.py
+++ analyzers/Splunk/splunk.py
@@ -36,7 +36,7 @@ class Splunk(Analyzer):
             self.service = client.connect(
                 host=self.HOST,
                 port=self.PORT,
-                protocol=self.PROTOCOL
+                protocol=self.PROTOCOL,
                 username=self.USERNAME,
                 password=self.PASSWORD,
                 owner=self.OWNER,
