c49ac707cc0135f498371820bccc91a6050dc7ea
Fabien B. <15647296+nusantara-self@users.noreply.github.com>
2024-10-29 10:19:58 +0900

    Update awx.py - Better error handling

--- responders/AWX/awx.py
+++ responders/AWX/awx.py
@@ -21,22 +21,42 @@ class AWX(Responder):
         payload = {
             'extra_vars': json.dumps(self.observable_all)
         }
-        print("start awx job")
         # Start the job
         job_start_endpoint = self.url + '/api/v2/job_templates/' + self.workflow_id + '/launch/'
+        
+        try:
+            response = requests.post(
+                job_start_endpoint,
+                headers=headers,
+                auth=(self.username, self.password),
+                data=json.dumps(payload),
+                verify=self.cert_path if self.cert_path else False
+            )
 
-        if self.cert_path == '':
-            response = requests.post(job_start_endpoint, headers=headers, auth=(self.username, self.password), data=json.dumps(payload))
-        else:
-            response = requests.post(job_start_endpoint, headers=headers, auth=(self.username, self.password), data=json.dumps(payload), verify=self.cert_path)
+            response.raise_for_status()
 
-        # Check the response status
-        if response.status_code == 201:
-            print("success")
-            self.report({"Message": "Executed workflow"})
-        else:
-            print("error")
-            self.error(response.status_code)
+            if response.status_code == 201:
+                self.report({"Message": "Executed AWX job successfully"})
+            else:
+                error_msg = response.json().get('detail', 'Unknown error')
+                self.error(f"AWX job execution returned unexpected status {response.status_code}: {error_msg}")
+        except requests.exceptions.SSLError as ssl_err:
+            self.error(f"SSL Error: {str(ssl_err)}")
+        except requests.exceptions.ConnectionError as conn_err:
+            self.error(f"Connection Error: {str(conn_err)}")
+        except requests.exceptions.Timeout as timeout_err:
+            self.error(f"Request Timeout: {str(timeout_err)}")
+        except requests.exceptions.RequestException as req_err:
+            try:
+                # Try to get additional details from the JSON response
+                error_details = response.json().get('detail', 'No additional error details available.')
+            except json.JSONDecodeError:
+                error_details = 'Failed to parse error details from response.'
+            
+            self.error(f"Request Error: {str(req_err)} - Details: {error_details}")
+        except Exception as unexpected_err:
+            self.error(f"Unexpected Error: {str(unexpected_err)}")
+
 
 if __name__ == '__main__':
     AWX().run()
