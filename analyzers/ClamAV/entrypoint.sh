fa13905c3d6ae8bf1f292805c6b3347598383b2f
nusantara-self <15647296+nusantara-self@users.noreply.github.com>
2025-04-03 12:47:03 +0800

    Fix ClamAV + Alpine Support

---
+++ analyzers/ClamAV/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+# create necessary directories with proper permissions
+mkdir -p /run/clamav && chown clamav:clamav /run/clamav && chmod 750 /run/clamav
+mkdir -p /var/lib/clamav
+
+# start freshclam in the background
+/usr/bin/freshclam --daemon &
+sleep 20  # Wait for definitions to update
+
+# start clamd in the background
+/usr/sbin/clamd &
+
+# start analyzer script
+exec python ClamAV/pyclam_analyzer.py
